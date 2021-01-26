20503c6302199c4f37f162e9620fdd58e5a288bd
tommaso lauria <tommaso.lauria9@gmail.com>
2021-01-26 20:12:52 +0100

    Add project files.

---
+++ Es10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Gestione;
+
+namespace Es10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool vf = false;
+            bool dS = false;
+            Console.WriteLine("vuoi effettuare il riempimento casuale o manuale");
+            string str = Console.ReadLine();
+            if (str == "casuale" || str == "Casuale" || str == "CASUALE")
+                vf = true;
+            ClasseArray.Riempimento(vf);
+            Console.WriteLine("vuoi stapare l'array da destra a sinista digita DS;\nse vuoi stamparlo da sinistra a destra SD");
+            string destra_Sinistra = Console.ReadLine();
+            if (destra_Sinistra == "DS"|| destra_Sinistra == "Ds"|| destra_Sinistra == "ds")
+                dS = true;
+            ClasseArray.OrdineStampa(dS);
+                
+        }
+    }
+}
---
+++ Gestione/ClasseArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Gestione
+{
+    public class ClasseArray
+    {
+        private const int a = 5;
+        private static int[] array = new int[a];
+        public static void Riempimento(bool vf)
+        {
+            Random r = new Random();
+            if (vf == true)
+            {
+                for(int i =0; i < array.Length; i++)
+                {
+                    array[i] = r.Next();
+                }
+            }
+            else
+            {
+                for(int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine($"inserisci il valore in posizione {i}");
+                    array[i] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        public static void OrdineStampa(bool destraSinistra)
+        {
+            if (destraSinistra == true)
+            {
+                for (int i = array.Length - 1; i >= 0; i--)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+            }
+            else
+            {
+                for(int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+            }
+
+        }
+    }
+}
